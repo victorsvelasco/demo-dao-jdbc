c34c0fade8951c8c18972c5771d4f348c4eaafc1
victorsvelasco <victorsvelasco@gmail.com>
2022-07-26 12:12:46 -0300

    Reusing instabtiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,7 @@ public class Program {
 				
 		SellerDao sellerDao = new DaoFactory().createSellerDao();
 		
+		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(7);
 		
 		System.out.println(seller);
